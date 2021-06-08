4a5d51c3a57a17b02f6b26c30942e1d667989773
Thiago Leão <73238835+ThiagoRSLeao@users.noreply.github.com>
2021-06-08 15:25:39 -0300

    Added 'grays' color possibility

--- app/Main.hs
+++ app/Main.hs
@@ -23,18 +23,32 @@ svgPath :: String -> String -> String
 svgPath d style = 
   printf "<path d='%s' style='%s' />\n" d style
 
-genCurves :: [(Int, Int)] -> [String] -> Int -> [String]
-genCurves pointList colors n = [svgCurvedPath (p !! (a*4)) (p !! ((a*4)+1)) (p !! ((a*4)+2)) (p !! ((a*4)+3)) (colors !! a)| a <- [0..n-1]] where p = pointList
+--Radom Svgs
 
-genPointList :: [Int] -> [Int] -> [(Int, Int)]
-genPointList listX listY = zip listX listY
-
+--Gera uma receita de inteiro aleatório entre x e y
 rndBet :: (Int,Int) -> IO Int
 rndBet (x,y) = randomRIO(x,y)
 
-svgCurvedPath :: (Int, Int) -> (Int, Int) -> (Int, Int) -> (Int, Int) -> String -> String
-svgCurvedPath (x, y) (cx, cy) (cx2, cy2) (x2, y2) color = svgPath ("M "++ show x ++ " " ++ show y ++ " " ++ show cx ++ " " ++ show cy ++ " " ++ show cx2 ++ " " ++ show cy2 ++ " L "++ show x2 ++ " " ++ show y2) ("stroke-width: 3;stroke: rgb"++ color ++";fill: none;")
+--Recebe duas listas de inteiros e associa cada valor irmão das listas em uma tupla (x,y)
+genPointList :: [Int] -> [Int] -> [(Int, Int)]
+genPointList listX listY = zip listX listY
 
+--Gera uma lista de Strings de tag svgPatch com curva;
+genCurves :: [(Int, Int)] -> [String] -> Int -> [String]
+genCurves pointList colors n = [svgCurvedPath (p !! (a*4)) (p !! ((a*4)+1)) (p !! ((a*4)+2)) (p !! ((a*4)+3)) (colors !! a)| a <- [0..n-1]] where p = pointList
+
+--Gera um svgPath curvado
+svgCurvedPath :: (Int, Int) -> (Int, Int) -> (Int, Int) -> (Int, Int) -> String -> String
+svgCurvedPath (x, y) (cx, cy) (cx2, cy2) (x2, y2) color = svgPath ("M "++ show x ++ " " ++ show y ++ " C " ++ show cx ++ " " ++ show cy ++ " " ++ show cx2 ++ " " ++ show cy2 ++ " " ++ show x2 ++ " " ++ show y2) ("stroke-width: 3;stroke: rgb"++ color ++";fill: none;")
+
+genBrokens :: [(Int, Int)] -> [String] -> Int -> [String]
+genBrokens pointList colors n = [svgBrokenPath (p !! (a*4)) (p !! ((a*4)+1)) (p !! ((a*4)+2)) (p !! ((a*4)+3)) (colors !! a)| a <- [0..n-1]] where p = pointList
+
+--Gera um path com quebras
+svgBrokenPath :: (Int, Int) -> (Int, Int) -> (Int, Int) -> (Int, Int) -> String -> String
+svgBrokenPath (x, y) (cx, cy) (cx2, cy2) (x2, y2) color = svgPath ("M "++ show x ++ " " ++ show y ++ " " ++ show cx ++ " " ++ show cy ++ " " ++ show cx2 ++ " " ++ show cy2 ++ " L " ++ show x2 ++ " " ++ show y2) ("stroke-width: 3;stroke: rgb"++ color ++";fill: none;")
+
+--gera uma lista de strings no formato (r,g,b) a partir de 3 listas de string e um número n de elementos.
 mergeColors :: [Int] -> [Int] -> [Int] -> Int -> [String]
 mergeColors reds blues greens n = ["("++ show (reds !! x) ++ ", " ++ show (greens !! x) ++ ", " ++ show (blues !! x) ++ ")" | x <- [0..n]]
 
@@ -46,6 +60,10 @@ main = do
     h <- getLine
     putStrLn "How much forms do you want?"  
     n <- getLine 
+    putStrLn "Broken lines or curved lines? type 'b' or 'c'"  
+    t <- getLine 
+    putStrLn "What about colors? type 'g' for grays or 'r' for raibowMayhem"
+    c <- getLine 
     --putStrLn "What should be the color? type as '(xxx,xxx,xxx)' or 'r' for color mayhem!"  
     --color <- getLine 
     --colorMayhem <- sequence [rndBet(0, 255) | x <- [0..((read n :: Int)*3)]]
@@ -55,12 +73,17 @@ main = do
 
     rxList <- sequence [rndBet(1, read w :: Int) | x <- [0..((read n :: Int)*4)]] 
     ryList <- sequence [rndBet(1, read h :: Int) | x <- [0..((read n :: Int)*4)]]
-    reds <- sequence [rndBet(1, read h :: Int) | x <- [0..(read n :: Int)]]
-    greens <- sequence [rndBet(1, read h :: Int) | x <- [0..(read n :: Int)]]
-    blues <- sequence [rndBet(1, read h :: Int) | x <- [0..(read n :: Int)]]    
+    reds <- sequence [rndBet(0, 255) | x <- [0..(read n :: Int)]]
+    greens <- sequence [rndBet(0, 255) | x <- [0..(read n :: Int)]]
+    blues <- sequence [rndBet(0, 255) | x <- [0..(read n :: Int)]]    
+    grays <- sequence [rndBet(0, 255) | x <- [0..(read n :: Int)]]    
     
 
-    writeFile "./Abstrart.svg" (svgBegin (read w :: Int) (read h :: Int) ++ (intercalate " " (genCurves (genPointList rxList ryList) (mergeColors reds blues greens (read n :: Int)) (read n :: Int))) ++ svgEnd)
+    writeFile "./Abstrart.svg" (svgBegin (read w :: Int) (read h :: Int) ++ (intercalate " " (if( t == "b") then genBrokens (genPointList rxList ryList) (if(c == "r") then (mergeColors reds blues greens (read n :: Int)) else (mergeColors reds reds reds (read n :: Int))) (read n :: Int) else genCurves (genPointList rxList ryList) ((if(c == "r") then (mergeColors reds blues greens (read n :: Int)) else (mergeColors reds reds reds (read n :: Int)))) (read n :: Int))) ++ svgEnd) 
+    
+    
+    --Implementar formas geométricas, case of e where.
+      
     --writeFile "./Abstrart.svg" (svgBegin (read w :: Int) (read h :: Int) ++ (svgPath ("M "++ show (getrnd (read w :: Int)) ++ " 100 L 150 200") "stroke-width: 3;stroke:red;fill: none;") ++ svgEnd)
 --getrnd x = do
     --r <- randomRIO(1, x)
